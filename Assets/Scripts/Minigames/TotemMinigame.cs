53cd0b3e72395f55737aae0f5d81a3577af0354a
WillowDWisp <devon.rogers1@ontariotechu.net>
2026-02-16 10:17:51 -0500

    Update totem minigame
    The script can now output whether or not the player has put the correct answer in for the totem puzzle. Currently it just outputs whether or not the answer is correct to the console, but eventually it will trigger different dialogue based on correctness and spawn a chest if correct.

--- Assets/Scripts/Minigames/TotemMinigame.cs
+++ Assets/Scripts/Minigames/TotemMinigame.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class TotemMinigame : MonoBehaviour
 {
+    [SerializeField] private GameObject TopPiece;
+    [SerializeField] private GameObject TopPieceSpot;
+    [SerializeField] private GameObject MiddlePiece;
+    [SerializeField] private GameObject MiddlePieceSpot;
+    [SerializeField] private GameObject BottomPiece;
+    [SerializeField] private GameObject BottomPieceSpot;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,40 @@ public class TotemMinigame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //debug method to check if the puzzle is complete. need to make it so interacting with the Chief triggers this code instead.
+        // also make it do more than outputting to the console
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (CheckTotemPieces())
+            {
+                Debug.Log("Correct!");
+            } else
+            {
+                Debug.Log("Incorrect.");
+            }
+        }
+    }
+
+    //Is this the most effective way to do this? Probably not. Does it work? It should.
+    public bool CheckTotemPieces()
+    {
+        //check if the totem pieces are in their corresponding spots
+
+        if (!TopPiece.GetComponent<Rigidbody2D>().IsTouching(TopPieceSpot.GetComponent<BoxCollider2D>()))
+        {
+            return false;
+        }
+
+        if (!MiddlePiece.GetComponent<Rigidbody2D>().IsTouching(MiddlePieceSpot.GetComponent<BoxCollider2D>()))
+        {
+            return false;
+        }
+
+        if (!BottomPiece.GetComponent<Rigidbody2D>().IsTouching(BottomPieceSpot.GetComponent<BoxCollider2D>()))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
